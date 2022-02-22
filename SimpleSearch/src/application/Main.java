2422e71b9cdebc8c00b012c21f2d26ba009bcf97
lywysocki <lywysocki@gmail.com>
2022-02-22 14:49:32 -0500

    prints HTML to JavaFX window

--- SimpleSearch/src/application/Main.java
+++ SimpleSearch/src/application/Main.java
@@ -1,5 +1,14 @@
 package application;
-	
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,13 +19,34 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		
+		String data = "";
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 
 			Data test = new Data();
-			String txt = test.Data("https://finance.yahoo.com/trending-tickers");
+			String txtFile = test.Data("https://finance.yahoo.com/trending-tickers");
+			
+			Path file = Paths.get(txtFile);
+			InputStream in = null;
+			StringBuffer cBuf = new StringBuffer();
+			try {
+			    in = Files.newInputStream(file);
+			    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			    String line = null;
+
+			    while ((line = reader.readLine()) != null) {
+			        System.out.println(line);
+			        cBuf.append("\n");
+			        cBuf.append(line);
+			    }
+			} catch (IOException x) {
+			    System.err.println(x);
+			} finally {
+			    if (in != null) in.close();
+			}
+
+			String txt = cBuf.toString();
 			
 			TextArea textArea = new TextArea(txt);
 			primaryStage.setScene(new Scene(textArea));
