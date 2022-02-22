4b49e750037194086f8e42d9df1f2b2ee0162fa2
lywysocki <lywysocki@gmail.com>
2022-02-22 13:04:40 -0500

    starting to scrape for stock data

--- SimpleSearch/src/application/Data.java
+++ SimpleSearch/src/application/Data.java
@@ -1,8 +1,23 @@
 package application;
-/*
- * Hello 2.0
- */
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Data {
+	final String URL = "https://finance.yahoo.com/trending-tickers"
 	
+	try {
+		Document doc = Jsoup.connect(URL).get();
+
+		for (Element row : ) {
+			
+		}
+
+
+	} catch (Exception ex) {
+		
+	}
 
 }
